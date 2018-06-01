6ecd64e464827d93c1c28474cd68e253a81bd499
Fabys <fabys@fabys.eu>
2018-06-01 16:41:57 +0200

    Cleanup timeout  middleware

--- middleware/timeout.go
+++ middleware/timeout.go
@@ -9,11 +9,13 @@ import (
 // Timeout creates a TimeoutHandler and updates the request context with the given timeout.
 func Timeout(timeout time.Duration) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
-		return http.TimeoutHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fn := func(w http.ResponseWriter, r *http.Request) {
 			ctx, cancel := context.WithTimeout(r.Context(), timeout)
 			defer cancel()
 
 			next.ServeHTTP(w, r.WithContext(ctx))
-		}), timeout, "Request Timeout")
+		}
+
+		return http.TimeoutHandler(http.HandlerFunc(fn), timeout, "Request Timeout")
 	}
 }
