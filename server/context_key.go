d49a770a18dc8e1b09d0d2620e51ad0e7dac5e8a
Fabys <fabys@fabys.eu>
2018-06-01 16:41:16 +0200

    Update context key

--- server/context_key.go
+++ server/context_key.go
@@ -3,12 +3,12 @@ package server
 // ContextKey is the type of the key for predefined context keys (e.g. request-id).
 // const RequestIdCtxKey = &ContextKey{"request-id"}
 type ContextKey struct {
-	name string
+	Name string
 }
 
 // String returns the string representation of the ContextKey.
 func (ck *ContextKey) String() string {
-	return "fabyscore-go_" + ck.name
+	return "fabyscore-go_" + ck.Name
 }
 
 // dynamicContextKey is the type of the key for dynamic context entries (Unknown string value as key. e.g. /hello/:name -> context entry with key 'name').
--- server/context_key_test.go
+++ server/context_key_test.go
@@ -10,5 +10,12 @@ func TestContextKey(t *testing.T) {
 	var testKey = &ContextKey{"test"}
 
 	assert.Equal(t, "fabyscore-go_test", testKey.String())
-	assert.Equal(t, "test", testKey.name)
+	assert.Equal(t, "test", testKey.Name)
+}
+
+func TestContextKeyUnique(t *testing.T) {
+	key1 := &ContextKey{"key1"}
+	key2 := &ContextKey{"key2"}
+
+	assert.NotEqual(t, key1, key2)
 }
